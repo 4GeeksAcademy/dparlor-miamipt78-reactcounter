b0243b2f2f886774cc283eac67f4022a0a06f853
Dabeldab <1dareios@gmail.com>
2025-04-19 06:10:40 +0000

    complete with a color randomizer

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,24 @@
 import React from "react";
+import SimpleCounter from './SimpleCounter';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function calculateSeconds(aCounter, placeValue) {
+    return Math.floor(aCounter / placeValue) % 10
+}
+
+const Home = ({counter,chosenColor= 'currentcolor'}) => {
+    return(
+        <>
+        <SimpleCounter 
+        thousandsDigit = {calculateSeconds(counter, 1000)}
+        hundredsDigit = {calculateSeconds(counter, 100)}
+        tensDigit = {calculateSeconds(counter, 10)}
+        onesDigit = {calculateSeconds(counter, 1)}
+        color ={chosenColor}
+        />
+        </>
+    )
+}
 
 export default Home;
